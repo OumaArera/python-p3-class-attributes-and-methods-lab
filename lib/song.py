82884879576797065023b34eab138b8b4608f732
OumaArera <johnouma999@gmail.com>
2024-03-05 15:05:40 +0300

    feat: complete

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,58 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Increment the count of songs
+        Song.add_song_to_count()
+
+        # Add genre and artist to respective lists and counts
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+# # Example Usage:
+# ninety_nine_problems = Song("99 Problems", "Jay-Z", "Rap")
+# single_ladies = Song("Single Ladies", "Beyonce", "Pop")
+
+# print(Song.count)  # Output: 2
+# print(Song.genres)  # Output: ['Rap', 'Pop']
+# print(Song.artists)  # Output: ['Jay-Z', 'Beyonce']
+# print(Song.genre_count)  # Output: {'Rap': 1, 'Pop': 1}
+# print(Song.artist_count)  # Output: {'Jay-Z': 1, 'Beyonce': 1}
